8fd189eb98f3558e910c12dda9211e6c21291ace
Diego Tenório <38768837+diegoptenorio@users.noreply.github.com>
2024-02-22 14:10:15 -0300

    debt: New logo format

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,4 +1,4 @@
-import logo from "../../assets/img/logo.png";
+import logo from "../../assets/img/logo.svg";
 
 export const Logo = () => (
     <img src={logo} className="logo" alt="logo" draggable={false} />
